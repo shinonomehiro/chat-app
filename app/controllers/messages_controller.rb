d3efa9e417bdb0c786127aa98268bac4a5107690
shinonomehiro <shinonomehiro500@gmail.com>
2021-01-20 21:45:33 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,5 @@
 class MessagesController < ApplicationController
+  
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
@@ -8,7 +9,6 @@ class MessagesController < ApplicationController
   def create
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
-    @message.save
     if @message.save
       redirect_to room_messages_path(@room)
     else
